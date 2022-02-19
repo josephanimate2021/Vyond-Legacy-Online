2beb78670ef628ff13c1d893a8052c53f55aa672
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-02-19 05:38:12 -0500

    Update save.js

--- starter/save.js
+++ starter/save.js
@@ -11,12 +11,9 @@ const http = require("http");
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || (url.path != "/goapi/saveTemplate/")) return;
 	loadPost(req, res).then(([data, mId]) => {
-		const trigAutosave = data.is_triggered_by_autosave;
-		if (trigAutosave && (!data.starterId || data.noAutosave)) return res.end("0");
-
 		var body = Buffer.from(data.body_zip, "base64");
 		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
-		starter.save(body, thumb, mId, data.presaveId).then((nId) => res.end("0" + nId));
+		starter.save(body, thumb).then((nId) => res.end("0" + nId));
 	});
 	return true;
 };
