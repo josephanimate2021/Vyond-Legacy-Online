715dbcf01379d1825727e620b345c4b607c5437c
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-02-19 05:58:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const mvu = require("./movie/upload");
 const asu = require("./asset/upload");
 const stl = require("./static/load");
 const stp = require("./static/page");
+const str = require("./starter/save");
+const stt = require("./starter/thmb");
 const asl = require("./asset/load");
 const asL = require("./asset/list");
 const ast = require("./asset/thmb");
@@ -24,7 +26,7 @@ const tsv = require("./tts/voices");
 const tsl = require("./tts/load");
 const url = require("url");
 
-const functions = [mvL, pmc, asl, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, stl];
+const functions = [mvL, pmc, asl, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, str, stt, stl];
 
 module.exports = http
 	.createServer((req, res) => {
