2e16f271b9da1400d12a42076218f4bbc008cf6f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-02-19 18:26:25 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,17 +15,21 @@ cls
 :::::::::::::::::::::::::::::::
 
 :: Check for installation
+echo Checking For Node.js Instalation...
 for /f "delims=" %%i in ('npm -v 2^>nul') do set output=%%i
 IF "!output!" EQU "" (
 	echo Node.js could not be found. please try again later
 	pause
 	exit
 ) else (
+        echo Node.js has been detected. Processing Bootup...
+	PING -n 6 127.0.0.1>nul
 	goto start
 )
 
 :: Run npm start
 :start
+cls
 echo Vyond Legacy Online is now starting...
 start /MIN open_nodejs.bat
 PING -n 6 127.0.0.1>nul
