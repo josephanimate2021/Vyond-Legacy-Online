a24e42195e35497106b30299ea7b649ea2146c85
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-02-19 22:03:25 -0500

    hm

--- asset/list.js
+++ asset/list.js
@@ -1,60 +1,54 @@
+// All rights and credits goes to David's TV Studio for helping me revive the starters feature.
 const loadPost = require("../misc/post_body");
 const header = process.env.XML_HEADER;
 const fUtil = require("../misc/file");
 const nodezip = require("node-zip");
 const base = Buffer.alloc(1, 0);
 const asset = require("./main");
-const http = require("http");
 const starter = require("../starter/main");
+const http = require("http");
 
 async function listAssets(data, makeZip) {
 	var xmlString;
+	var files;
 	switch (data.type) {
 		case "char": {
 			const chars = await asset.chars(data.themeId);
 			xmlString = `${header}<ugc more="0">${chars
 				.map(
 					(v) =>
-						`<char id="${v.id}" name="Untitled" cc_theme_id="${v.theme}" thumbnail_url="/char_thumbs/${v.id}.png" copyable="Y"><tags/></char>`
+						`<char id="${v.id}" name="Untitled" cc_theme_id="${v.theme}" thumbnail_url="http://localhost:4343/char_thumbs/${v.id}.png" copyable="Y"><tags/></char>`
 				)
 				.join("")}</ugc>`;
 			break;
 		}
 		case "bg": {
-			var files = asset.list(data.movieId, "bg");
+			files = asset.list(data.movieId, "bg");
 			xmlString = `${header}<ugc more="0">${files
 				.map((v) => `<background subtype="0" id="${v.id}" name="${v.name}" enable="Y"/>`)
 				.join("")}</ugc>`;
 			break;
 		}
+		
 		case "sound": {
-			var files = asset.list(data.movieId, "voiceover");
+			files = asset.list(data.movieId, "sound");
 			xmlString = `${header}<ugc more="0">${files
-				.map(
-					(v) =>
-						`<sound subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" duration="${v.duration}" downloadtype="progressive"/>`
-				)
+				.map((v) =>`<sound subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" duration="${v.duration}" downloadtype="progressive"/>`)
 				.join("")}</ugc>`;
 			break;
-		}	
+		}
 		case "movie": {
-			var files = starter.list();
+			files = starter.list()
 			xmlString = `${header}<ugc more="0">${files
-				.map(
-					(v) =>
-						`<movie id="${v.id}" enc_asset_id="${v.id}" path="/_SAVED/${v.id}" numScene="1" title="${v.name}" thumbnail_url="/starter_thumbs/${v.id}.png"><tags></tags></movie>`
-				)
+				.map((v) =>`<movie id="${v.id}" path="/_SAVED/${v.id}" numScene="1" title="${v.name}" thumbnail_url="/starter_thumbs/${v.id}.png"><tags></tags></movie>`)
 				.join("")}</ugc>`;
 			break;
 		}
 		case "prop":
 		default: {
-			var files = asset.list(data.movieId, "prop");
+			files = asset.list(data.movieId, "prop");
 			xmlString = `${header}<ugc more="0">${files
-				.map(
-					(v) =>
-						`<prop subtype="0" id="${v.id}" name="${v.name}" enable="Y" holdable="0" headable="0" placeable="1" facing="left" width="0" height="0" duration="0"/>`
-				)
+				.map((v) =>`<prop subtype="0" id="${v.id}" name="${v.name}" enable="Y" holdable="0" headable="0" placeable="1" facing="left" width="0" height="0" duration="0"/>`)
 				.join("")}</ugc>`;
 			break;
 		}
