86b4ec6ab7417f827fff882656b32ec0ed6eda9b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-26 19:53:53 -0400

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -72,16 +72,20 @@ module.exports = function (req, res, url) {
 
 			switch (data.type) {
 				case 'bg': {
-					for (let c = 0; c < files.length; c++) {
-						const file = files[c];
-						fUtil.addToZip(zip, `bg/${file.id}`, asset.loadLocal(file.id));
-					}
+					fUtil.addToZip(zip, 'bg/666.jpg', fs.readFileSync(`./pages/img/logo.png`));
+					break;
+				}
+				case 'prop': {
+					fUtil.addToZip(zip, 'prop/666.jpg', fs.readFileSync(`./pages/img/logo.png`));
 					break;
 				}
 			};
-			return Buffer.concat([base, await zip.zip()]);
-		} else {
-			return Buffer.from(xmlString);
+			res.setHeader('Content-Type', 'application/zip');
+			res.end(Buffer.concat([base, await zip.zip()]));
+		}
+		else {
+			res.setHeader('Content-Type', 'text/xml');
+			res.end(xmlString);
 		}
 	});
 	return true;
