bdbd99321ae9c233986e7a486f6b5858dfe747a2
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-26 16:48:03 -0400

    Create list.js

---
+++ starter/list.js
@@ -0,0 +1,7 @@
+const movie = require('../main');
+const starter = require('./main');
+module.exports = function (req, res, url) {
+	if (req.method != 'GET' || url.path != '/starterList') return;
+	Promise.all(movie.listStarter().map(starter.meta)).then(a => res.end(JSON.stringify(a)));
+	return true;
+}
