9664b2e5123c01d503b60ccfd6fec92c6fadcdd5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-26 16:40:58 -0400

    Delete meta.js

--- starter/meta.js
+++
@@ -1,8 +0,0 @@
-const starter = require('./main');
-module.exports = function (req, res, url) {
-	if (req.method != 'GET' || !url.path.startsWith('/starters/meta')) return;
-	starter.meta(url.path.substr(url.path.lastIndexOf('/') + 1))
-		.then(v => res.end(JSON.stringify(v)))
-		.catch(() => { res.statusCode = 404; res.end() });
-	return true;
-}
