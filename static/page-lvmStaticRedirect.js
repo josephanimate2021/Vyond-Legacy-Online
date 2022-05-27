17b4f7f5b355d6a1a81e2d855a2b6f60717ca275
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-26 20:21:08 -0400

    Create page-lvmStaticRedirect.js

---
+++ static/page-lvmStaticRedirect.js
@@ -0,0 +1,31 @@
+module.exports = function (req, res, url) {
+	if (req.method != 'GET') return;
+	const query = url.query;
+
+	var attrs, params, server, ut, redirectUrl;
+	switch (url.pathname) {
+		case '/videomaker/full/': {     
+			params = {
+				flashvars: {
+					'theme': '',
+				},
+			};
+			break;
+                }
+		default:
+			return;
+	}
+	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+	Object.assign(params.flashvars, query);
+	res.end(`<html>
+	<head>
+		<script>
+			function genorateId() { 
+				window.location = '/studio?tray=${params.flashvars.theme}'; 
+			}
+		</script>
+	</head>
+	<body onload="genorateId()"></body>
+</html>`);
+	return true;
+}
