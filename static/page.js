0eecca2e806be43df2e3e72e5c3eb000f39c3f9e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-26 20:10:29 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -35,7 +35,7 @@ module.exports = function (req, res, url) {
 		redirectUrl = `${server}/lvm-static/offline-player?movieId=`;
 	}
 	switch (url.pathname) {
-		case '/videomaker/full/editcheck/': {
+		case '/studio': {
                         let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
 				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
 			attrs = {
@@ -55,27 +55,6 @@ module.exports = function (req, res, url) {
 			sessions.set({ movieId: presave }, req);
 			break;
                 }
-		case '/videomaker/full/': {
-			let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
-				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
-			attrs = {
-				data: process.env.SWF_URL + '/go_full.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
-			};
-			params = {
-				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
-					'ut': ut, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
-					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'movieId': '',
-					'animationPath': process.env.SWF_URL + '/',
-				},
-				allowScriptAccess: 'always',
-			};
-			sessions.set({ movieId: presave }, req);
-			break;
-		}
-
 		default:
 			return;
 	}
