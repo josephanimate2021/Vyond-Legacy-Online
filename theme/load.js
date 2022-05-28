2424ef0331c789d5f390a66260757ea5b8777d41
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-27 23:04:54 -0400

    Update load.js

--- theme/load.js
+++ theme/load.js
@@ -5,15 +5,19 @@ const fUtil = require('../fileUtil');
 module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/goapi/getTheme/') return;
 	loadPost(req, res).then(data => {
-		var theme = data.themeId;
-		switch (theme) {
-			case 'family':
-				theme = 'custom';
-				break;
+		try {
+			var theme = data.themeId;
+			switch (theme) {
+				case 'family':
+					theme = 'custom';
+					break;
+			}
+			res.setHeader('Content-Type', 'application/zip');
+			fUtil.zippy(`${folder}/${theme}.xml`, 'theme.xml').then(b => res.end(b));
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+		} catch (err) {
+			console.error(`Error Loading Theme: ${data.themeId}`);
 		}
-		res.setHeader('Content-Type', 'application/zip');
-		fUtil.zippy(`${folder}/${theme}.xml`, 'theme.xml').then(b => res.end(b));
-		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 	});
 	return true;
 }
