5e1043abf1db7f8f0a3cf8cd382b1a0a9bc49fa4
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-27 23:11:47 -0400

    Delete list.js

--- starter/list.js
+++
@@ -1,7 +0,0 @@
-const movie = require('../movie/main');
-const starter = require('./main');
-module.exports = function (req, res, url) {
-	if (req.method != 'GET' || url.path != '/starterList') return;
-	Promise.all(movie.listStarter().map(starter.meta)).then(a => res.end(JSON.stringify(a)));
-	return true;
-}
