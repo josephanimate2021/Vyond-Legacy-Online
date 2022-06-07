3799e2155f2ad290389caf87918ca24eb07c96f9
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-06-07 11:01:30 -0400

    Delete update.bat

--- update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist .git ( goto nogit ) else ( goto yesgit )
-:nogit
-echo Sorry, git cannot be found. please use the batch file in this lvm project to clone this repo and try again.
-:yesgit
-echo Updating Vyond Legacy Online...
-git pull
